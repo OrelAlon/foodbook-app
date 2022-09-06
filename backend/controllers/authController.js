722e0c03c815afc257fd98627e5e31e058ad75c2
OrelAlon <orela231089@gmail.com>
2022-09-06 13:04:46 +0300

    add login route and bcrypt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,13 +1,18 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 //
 const register = async (req, res) => {
   try {
+    // bcrypt for new password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,
     });
+
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
@@ -16,6 +21,22 @@ const register = async (req, res) => {
 };
 
 //
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  try {
+    // find user
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 module.exports = { register, login };

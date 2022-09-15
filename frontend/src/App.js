8afa7f66c692aec923f5c4a4cd66902b5e88528a
orelalon <orela231089@gmail.com>
2022-09-15 09:51:23 +0300

    add ProtectedRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,53 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import Register from "./pages/register/Register.jsx";
 import Login from "./pages/login/Login.jsx";
 import Home from "./pages/home/Home.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import AddRestaurant from "./pages/addRestaurant/AddRestaurant.jsx";
+
 import "./App.css";
 
 function App() {
-  // useEffect(() => {
-  //   const fetchPost = async () => {
-  //     const res = await axios.get("/restaurants/restaurants");
-  //     console.log(res.data);
-  //   };
-  //   fetchPost();
-  // }, []);
+  const ProtectedRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+    if (!user) {
+      return <Navigate to='/login' />;
+    }
+
+    return children;
+  };
 
   return (
-    <div className='App'>
-      <Router>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-
-          <Route path='/register' element={<Register />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/profile/:username' element={<Profile />} />
-          <Route path='/addrestaurant' element={<AddRestaurant />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <div className='App'>
+        <Router>
+          <Routes>
+            <Route path='/'>
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/profile/:username' element={<Profile />} />
+              <Route path='/addrestaurant' element={<AddRestaurant />} />
+            </Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 

c3e4ff223d92d765673d1156f70cf802bad0ef42
OrelAlon <orela231089@gmail.com>
2022-12-05 15:00:54 +0200

    noAvatar img in share post and post

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -6,7 +6,6 @@ import moment from "moment";
 import axios from "axios";
 
 import heart from "../../assets/heart.png";
-// import { Restaurants } from "../../dummyData";
 
 import "./post.css";
 
@@ -58,7 +57,7 @@ const Post = ({ post }) => {
                 src={
                   user.profilePicture
                     ? PF + user.profilePicture
-                    : PF + "noAvatar.png"
+                    : "https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
                 }
                 alt=''
               />
--- frontend/src/components/sharePost/SharePost.js
+++ frontend/src/components/sharePost/SharePost.js
@@ -60,7 +60,11 @@ const SharePost = () => {
         <div className='shareTop'>
           <img
             className='shareProfileImg'
-            src={user.profilePicture ? PF + user.profilePicture : noAvatar}
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : "https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+            }
             alt=''
           />
 

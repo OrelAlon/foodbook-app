6fa991c798e5c2a4478adb77b9e497b3980a35da
OrelAlon <orela231089@gmail.com>
2022-12-14 16:23:28 +0200

    css post

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -6,6 +6,8 @@ import noAvatar from "../../assets/noAvatar.png";
 import moment from "moment";
 import axios from "axios";
 
+import { Image } from "@mantine/core";
+
 import "./post.css";
 
 const Post = ({ post }) => {

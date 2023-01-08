52c39b7707e12413b4183b36c0578a1fa29dea43
OrelAlon <orela231089@gmail.com>
2023-01-08 23:10:52 +0200

    create grid images component

--- frontend/src/pages/Test/Test.jsx
+++ frontend/src/pages/Test/Test.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./test.css";
 
 const Test = () => {
+  const [data, setData] = useState([]);
+  console.log(data);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(`/api/posts/feed`);
+      setData(res.data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <div>
-      <div> Test</div>
+    <div className='image-grid'>
+      <section className='image-grid-list'>
+        {data.map((image, index) => {
+          return (
+            <a href='' className='image-grid' key={index}>
+              <figure className='image-grid-img'>
+                <img src={image.img} alt={image.img} />
+              </figure>
+              <span className='image-grid-overlay'>
+                <p>
+                  <span className='image-grid-likes'>150</span>
+                  <span className='image-grid-comments'>10</span>
+                </p>
+              </span>
+            </a>
+          );
+        })}
+      </section>
     </div>
   );
 };

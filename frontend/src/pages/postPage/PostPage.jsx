b0c0cbd65e18644cd63f8d247e6e8d1f78f7f41c
OrelAlon <orela231089@gmail.com>
2023-01-10 22:00:50 +0200

    handle the frontent fetch post data

--- frontend/src/pages/postPage/PostPage.jsx
+++ frontend/src/pages/postPage/PostPage.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router";
+
 import { Image } from "@mantine/core";
 
 const PostPage = () => {
+  const [post, setPost] = useState({});
+
+  const postIdParams = useParams().id;
+  console.log(postIdParams);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await axios.get(`/api/post/?id=${postIdParams}`);
+      setPost(res.data);
+    };
+    fetchPost();
+  }, [usernameParams]);
+
   return (
     <div>
       PostPage

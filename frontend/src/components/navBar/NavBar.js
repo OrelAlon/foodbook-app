8c051dca609f6ebe9c6b06d7ae53091f1a74017c
OrelAlon <orela231089@gmail.com>
2023-01-14 17:30:44 +0200

    revert last commit

--- frontend/src/components/navBar/NavBar.js
+++ frontend/src/components/navBar/NavBar.js
@@ -4,8 +4,6 @@ import { useLocation } from "react-router";
 import ShareImageModal from "../shareImageModal/ShareImageModal";
 import NavMenu from "../navMenu/NavMenu";
 import Logo from "../logo/Logo";
-import logoTran from "../../assets/transparent.png";
-import grid from "../../assets/grid.svg";
 
 const NavBar = ({ setShowGrid, showGrid }) => {
   const [shareImageOpened, setShareImageOpened] = useState(false);
@@ -23,16 +21,15 @@ const NavBar = ({ setShowGrid, showGrid }) => {
             onClick={() => setShowGrid(!showGrid)}
             className='add-image-btn transform'
           >
-            {showGrid ? <img width='85%' src={grid} alt='' /> : "💻"}
+            {showGrid ? "📲" : "💻"}
           </button>
         )}
         <button
           onClick={() => setShareImageOpened(true)}
           className='add-image-btn transform'
         >
-          <img width='100%' src={logoTran} alt='' />{" "}
+          📷
         </button>
-
         <NavMenu />
       </div>
       <ShareImageModal

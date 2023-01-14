bbdd661f81e767df32d524fd3a80c8d8ac112b4f
OrelAlon <orela231089@gmail.com>
2023-01-14 15:30:07 +0200

    adding handle exists user

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
       navigate("/");
     } catch (error) {
       setLoading(false);
-      setErrorMsg(error.response.data);
+      setErrorMsg(error.response.data.message);
     }
   };
   const handleGoogleLogin = async (response) => {
@@ -47,7 +47,7 @@ const Login = () => {
       navigate("/");
     } catch (error) {
       setLoading(false);
-      setErrorMsg(error.response.data);
+      setErrorMsg(error.response.data.message);
     }
   };
 

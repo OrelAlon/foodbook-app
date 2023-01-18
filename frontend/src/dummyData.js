7e395f3800435558da2a199fdb06b758f2b04115
OrelAlon <orela231089@gmail.com>
2023-01-19 00:49:50 +0200

    error with deploy

--- frontend/src/dummyData.js
+++ frontend/src/dummyData.js
@@ -1,57 +1,9 @@
-export const Posts = [
-  {
-    _id: 1,
-    desc: "Love For All, Hatred For None.",
-    img: "https://media.moddb.com/cache/images/members/5/4550/4549205/thumb_620x2000/duck.jpg",
-    date: "5 mins ago",
-    userId: 1,
-    like: 32,
-    comment: 9,
-  },
-
-  {
-    _id: 4,
-    desc: "Work in a JOB you LOVE!",
-
-    img: "https://images.unsplash.com/photo-1513542789411-b6a5d4f31634?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
-    date: "4 hours ago",
-    userId: 4,
-    like: 7,
-    comment: 3,
-  },
-  {
-    _id: 5,
-    desc: "so cool img of Orel Alon",
-
-    img: "https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-    date: "5 hours ago",
-    userId: 5,
-    like: 23,
-    comment: 5,
-  },
-];
-export const Restaurants = [
-  {
-    _id: 2,
-    profilePicture:
-      "https://bestrest.rest/wp-content/uploads/2019/03/1ARIA_FINAL_LOGO-%D7%A2%D7%95%D7%AA%D7%A7.jpg",
-    restaurantname: "ARIA",
-  },
-  {
-    _id: 3,
-    profilePicture:
-      "https://res.cloudinary.com/ontopo/image/upload/c_crop,x_0,y_0,w_376,h_376/v1632298624/assets/VuHr8kl6Gse/poyE-rXSjD6/1.png",
-    restaurantname: "a",
-  },
-  {
-    _id: 4,
-    profilePicture: "https://media.easy.co.il/images/PICS/4822824.jpg",
-    restaurantname: "Emesh",
-  },
-  {
-    _id: 5,
-    profilePicture:
-      "https://www.ocdtlv.com/wp-content/uploads/2017/01/logo-main.png",
-    restaurantname: "OCD",
-  },
-];
+const deleteUser = async (req, res) => {
+  try {
+    await Post.deleteMany({ user: req.params.id });
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User and their posts have been deleted");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};

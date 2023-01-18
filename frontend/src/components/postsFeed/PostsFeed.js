ffd9c4aad0bb32e7ab207660a7e5fb8ea08edb53
OrelAlon <orela231089@gmail.com>
2023-01-18 18:22:36 +0200

    change structure

--- frontend/src/components/postsFeed/PostsFeed.js
+++ frontend/src/components/postsFeed/PostsFeed.js
@@ -1,14 +1,18 @@
-import React from "react";
-
 import Post from "../post/Post";
+import GridFeed from "../gridFeed/GridFeed";
 
-const PostsFeed = ({ posts }) => {
-  console.log(posts);
+const PostsFeed = ({ posts, showGrid }) => {
   return (
     <div>
-      {posts.map((p) => (
-        <Post key={p._id} post={p} />
-      ))}
+      {" "}
+      <div className={`${showGrid && "active"}`}>
+        <GridFeed images={posts} />
+      </div>
+      <div className={`${!showGrid && "active"}`}>
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
+        ))}
+      </div>
     </div>
   );
 };

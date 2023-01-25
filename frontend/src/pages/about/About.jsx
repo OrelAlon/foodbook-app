c9a05a16a9945f9b2c0256bfbc490d87d8921716
OrelAlon <orela231089@gmail.com>
2023-01-25 11:34:35 +0200

    about me page

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,5 +1,9 @@
 import NavBar from "../../components/navBar/NavBar";
 import logo from "../../assets/transparent.png";
+import git from "../../assets/git.png";
+import link from "../../assets/link.png";
+import me from "../../assets/me.jpg";
+import res from "../../assets/ress.png";
 import {
   IconBrandLinkedin,
   IconBrandGithub,
@@ -11,35 +15,43 @@ const About = () => {
   return (
     <>
       <NavBar />
-      <div className='about-div'>
-        <div className='about-text'>
-          <img src={logo} alt='logo' className='about-logo' />
+      <div className='about-me'>
+        <div className='about-me__header'>
+          <h1 className='about-me__title'>About Me</h1>
+          <img src={me} alt='Profile' className='about-me__img' />
+        </div>
+        <div className='about-me__text'>
+          <p>
+            Starting as a small project, it has grown into a real platform for
+            food lovers to discover and share their culinary experiences.
+          </p>
+          <p>
+            As a developer, I constantly strive to improve the user experience
+            and am proud to utilize the MERN stack to realize this vision
+          </p>
+        </div>
 
-          <h4 style={{ fontWeight: 800 }}>
-            this fun app
-            <br />
-            was built by me
-          </h4>
-          <h3 style={{ fontWeight: 800 }}>My CV</h3>
-
-          <a
-            target='_blank'
-            href='https://drive.google.com/file/d/1cmnvZl-lBD85br5w5tJORF-F6hEz8Vlc/view?usp=share_link'
-          >
-            <IconFileInfo className='cv' size={50} />
-          </a>
-          <h3 style={{ fontWeight: 800 }}>My LinkedIn</h3>
-
+        <div className='about-me__links'>
           <a
             target='_blank'
             href='https://www.linkedin.com/in/orel-alon-090689184'
+            className='about-me__link'
           >
-            <IconBrandLinkedin className='linkedin' size={50} />
+            <img src={link} alt='Profile' className='about-me__img' />{" "}
           </a>
-          <h3 style={{ fontWeight: 800 }}>My project's code</h3>
-
-          <a target='_blank' href='https://github.com/OrelAlon/foodbook-app'>
-            <IconBrandGithub className='github' size={50} />
+          <a
+            target='_blank'
+            href='https://github.com/OrelAlon/foodbook-app'
+            className='about-me__link'
+          >
+            <img src={git} alt='Profile' className='about-me__img' />{" "}
+          </a>
+          <a
+            target='_blank'
+            href='https://drive.google.com/file/d/1DU_z9W7dIIeLniNXZR10N7M85ZA5X7u9/view?usp=sharing'
+            className='about-me__link'
+          >
+            <img src={res} alt='Profile' className='about-me__img' />{" "}
           </a>
         </div>
       </div>

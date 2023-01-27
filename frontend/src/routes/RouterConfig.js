a5e4b83ce74583af1d5fd6ab7f6f80a11a578622
OrelAlon <orela231089@gmail.com>
2023-01-27 14:51:12 +0200

    unshow downbar in login/register pages

--- frontend/src/routes/RouterConfig.js
+++ frontend/src/routes/RouterConfig.js
@@ -1,66 +1,17 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "../pages/home/Home";
+import MainLayoutRoutes from "./MainLayuotRoutes";
 import Login from "../pages/login/Login";
-import About from "../pages/about/About";
 import Register from "../pages/register/Register";
-import UserProfile from "../pages/userProfile/UserProfile";
-import PostPage from "../pages/postPage/PostPage";
-import AllItemsPage from "../pages/allItemsPage/AllItemsPage";
-import RestaurantProfile from "../pages/restaurantProfile/RestaurantProfile";
-import EditRestaurantPage from "../pages/editRestaurantPage/EditRestaurantPage";
-import EditProfileUserPage from "../pages/editProfileUserPage/EditProfileUserPage";
-import EditPostPage from "../pages/editPostPage/EditPostPage";
-import RatingStarsPage from "../pages/ratingStarsPage/RatingStarsPage";
-
-import DownBar from "../components/downBar/DownBar";
-
-import { ProtectedRoute } from "./ProtectedRoute";
-import React from "react";
 
 const RouterConfig = () => {
-  const [showGrid, setShowGrid] = useState(false);
-
   return (
     <Router>
       <Routes>
-        <Route path='/'>
-          <Route
-            index
-            element={
-              <ProtectedRoute>
-                <Home showGrid={showGrid} />
-              </ProtectedRoute>
-            }
-          />
-          <Route path='/login' element={<Login />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/profile/:username' element={<UserProfile />} />
-          <Route path='/post/:id' element={<PostPage />} />
-          <Route path='/users' element={<AllItemsPage type={"users"} />} />
-          <Route
-            path='/restaurants'
-            element={<AllItemsPage type={"restaurants"} />}
-          />
-          <Route
-            path='/restaurant/:restaurantname'
-            element={<RestaurantProfile />}
-          />
-          <Route
-            path='/editrestaurant/:restaurantname'
-            element={<EditRestaurantPage />}
-          />
-          <Route
-            path='/editprofile/:username'
-            element={<EditProfileUserPage />}
-          ></Route>
-          <Route path='/editpost/:id' element={<EditPostPage />}></Route>
-          <Route path='/ratingstars' element={<RatingStarsPage />}></Route>
-        </Route>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<MainLayoutRoutes />} />
       </Routes>
-      <DownBar showGrid={showGrid} setShowGrid={setShowGrid} />
     </Router>
   );
 };

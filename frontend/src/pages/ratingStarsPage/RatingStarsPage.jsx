a65ad5fe06d7068a98ca3d183f8035f8b9fad3e0
OrelAlon <orela231089@gmail.com>
2023-01-28 13:00:28 +0200

    RatingStarsPage working with sort data

--- frontend/src/pages/ratingStarsPage/RatingStarsPage.jsx
+++ frontend/src/pages/ratingStarsPage/RatingStarsPage.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import { fetchAllUsers } from "../../api/ApiFetch";
 
 const RatingStarsPage = () => {
+  const [data, setData] = useState();
+
+  console.log(data);
+
+  const fetchDate = async () => {
+    const res = await fetchAllUsers();
+    setData(res.data.sort((a, b) => b.stars.length - a.stars.length));
+  };
+  useEffect(() => {
+    fetchDate();
+  }, []);
+
   return (
     <div>
       RatingStarsPage

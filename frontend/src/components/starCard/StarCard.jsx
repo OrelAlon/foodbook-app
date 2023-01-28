e2fc643a9c7e5b77abff817734d3c864b989b4d2
OrelAlon <orela231089@gmail.com>
2023-01-28 17:19:39 +0200

    finish function at stars page

--- frontend/src/components/starCard/StarCard.jsx
+++ frontend/src/components/starCard/StarCard.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useContext } from "react";
 
 import { AuthContext } from "../../context/AuthContext";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 import StarBtn from "../starBtn/StarBtn";
 
@@ -8,8 +10,10 @@ import noAvatar from "../../assets/noAvatar.png";
 
 const StarCard = ({ item }) => {
   const [isStar, setIsStar] = useState();
+  const [loading, setLoading] = useState(false);
+  const [star, setStar] = useState();
 
-  const { username, restaurantname, profilePicture, stars } = item;
+  const { _id, username, restaurantname, profilePicture, stars } = item;
 
   const { user: currentUser } = useContext(AuthContext);
 
@@ -17,16 +21,43 @@ const StarCard = ({ item }) => {
 
   useEffect(() => {
     setIsStar(checkIfStar);
+    setStar(stars.length);
   }, [checkIfStar]);
+
+  const starHandler = async () => {
+    setLoading(true);
+    const data = username ? "users" : "restaurants";
+    try {
+      await axios.put(`/api/${data}/${_id}/star`, {
+        userId: currentUser._id,
+      });
+      if (!isStar) {
+        setStar((prevStar) => prevStar + 1);
+
+        setIsStar(true);
+      } else if (isStar) {
+        setStar((prevStar) => prevStar - 1);
+        setIsStar(false);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className='star-card'>
       <div className='star cursor transform'>
-        <StarBtn isStar={isStar} />
+        <StarBtn isStar={isStar} starHandler={starHandler} loading={loading} />
       </div>
-      <img className='star-image' src={profilePicture || noAvatar} alt='' />
-      <div className='star-name'>{username || restaurantname}</div>
-      <div className='star-count'>with {stars.length} stars</div>
+      <Link
+        to={username ? `/profile/${username}` : `/restaurant/${restaurantname}`}
+        style={{ textDecoration: "none" }}
+      >
+        <img className='star-image' src={profilePicture || noAvatar} alt='' />{" "}
+      </Link>
+      <div className='star-name'>{username || restaurantname}</div>{" "}
+      <div className='star-count'>with {star} stars</div>
     </div>
   );
 };

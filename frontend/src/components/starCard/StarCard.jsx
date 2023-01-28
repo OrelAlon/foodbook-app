9463e05d0e6fa3a857a9ab50ac91e7fc7b7d59a0
OrelAlon <orela231089@gmail.com>
2023-01-28 16:06:00 +0200

    cosmetic to all the star component

--- frontend/src/components/starCard/StarCard.jsx
+++ frontend/src/components/starCard/StarCard.jsx
@@ -1,3 +1,9 @@
+import { useState, useEffect, useContext } from "react";
+
+import { AuthContext } from "../../context/AuthContext";
+
+import StarBtn from "../starBtn/StarBtn";
+
 import noAvatar from "../../assets/noAvatar.png";
 
 const StarCard = ({ user }) => {
@@ -6,12 +12,12 @@ const StarCard = ({ user }) => {
 
   return (
     <div className='star-card'>
-      <div className='star'>⭐</div>
+      <div className='star'>
+        <StarBtn />
+      </div>
       <img className='star-image' src={profilePicture || noAvatar} alt='' />
       <div className='star-name'>{username}</div>
-      <div className='star-count'>
-        <p>with {stars.length} stars</p>
-      </div>
+      <div className='star-count'>with {stars.length} stars</div>
     </div>
   );
 };

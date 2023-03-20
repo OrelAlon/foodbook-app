ce72c9de9ed38f48c871b761cbb3e99bdff3ac0b
OrelAlon <orela231089@gmail.com>
2023-03-20 10:58:22 +0200

    deleting comments

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -49,12 +49,8 @@ const createPost = async (req, res) => {
 const updatePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    // if (post.userId === req.body.userId) {
     await post.updateOne({ $set: req.body });
     res.status(200).json("this post has been updated");
-    // } else {
-    //   res.status(403).json("you can update only your post");
-    // }
   } catch (err) {
     res.status(500).json(err);
   }

f2e8f35c4d6825843250df60f5367db2efb7d3a8
OrelAlon <orela231089@gmail.com>
2023-03-29 23:51:50 +0300

    cosmetic to alart width

--- frontend/src/routes/RouterConfig.js
+++ frontend/src/routes/RouterConfig.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayoutRoutes from "./MainLayuotRoutes";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
+import About from "../pages/about/About";
 
 const RouterConfig = () => {
   return (
@@ -11,6 +12,7 @@ const RouterConfig = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='*' element={<MainLayoutRoutes />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </Router>
   );

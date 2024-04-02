f8d6828d940ea55e681b7fdd5c2d61fe34b6b4e6
OrelAlon <orela231089@gmail.com>
2024-04-02 16:20:35 +0300

    me remove

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -132,7 +132,7 @@ const Login = () => {
         </div>
         {/* </div> */}
         <div className='bottom_ transform'>
-          © 2023 - <span onClick={() => navigate('/about')}>About Me</span>
+          © 2023 - <span onClick={() => navigate('/about')}>About</span>
         </div>
       </div>
     </div>

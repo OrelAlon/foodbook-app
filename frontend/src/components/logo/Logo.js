e230b23fdd888d224ab24b4fd21586ecf7cad626
OrelAlon <orela231089@gmail.com>
2024-07-22 13:20:26 +0300

    handle reload when push the logo

--- frontend/src/components/logo/Logo.js
+++ frontend/src/components/logo/Logo.js
@@ -1,18 +1,17 @@
-import React from "react";
-
-import { Link } from "react-router-dom";
-
-import foodbook from "../../assets/foodbook.png";
-
-import "../../App.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import foodbook from '../../assets/foodbook.png';
+import '../../App.scss';
 
 function Logo() {
+  const handleClick = () => {
+    window.location.reload();
+  };
+
   return (
-    <>
-      <Link to='/' style={{ textDecoration: "none" }}>
-        <img className='logo' src={foodbook} alt='foodbook' />
-      </Link>
-    </>
+    <Link to='/' style={{ textDecoration: 'none' }} onClick={handleClick}>
+      <img className='logo' src={foodbook} alt='foodbook' />
+    </Link>
   );
 }
 

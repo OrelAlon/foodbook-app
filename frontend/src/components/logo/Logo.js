fca5ddea2630837eedd61a816367d451122fcf28
OrelAlon <orela231089@gmail.com>
2024-07-22 14:51:24 +0300

    combine restart to the logo btn

--- frontend/src/components/logo/Logo.js
+++ frontend/src/components/logo/Logo.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import foodbook from '../../assets/foodbook.png';
 import '../../App.scss';
 
 function Logo() {
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    window.location.reload();
+    navigate('/'); // Navigate to the main page
+    window.location.reload(); // Reload the page
   };
 
   return (
-    <Link to='/' style={{ textDecoration: 'none' }} onClick={handleClick}>
+    <div onClick={handleClick} style={{ cursor: 'pointer' }}>
       <img className='logo' src={foodbook} alt='foodbook' />
-    </Link>
+    </div>
   );
 }
 
